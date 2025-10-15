c00218ba346ca7b772e9f82c49eb92485029d236
Enrique Minguet Cobos <enriqueminguetapuntesdam@gmail.com>
2025-10-15 09:52:20 +0200

    V0.1: La pantalla de inicio existe y es similar a la idea (prob. vendria bien retoques visuales en un futuro)

--- app/src/main/java/com/example/practica_kotlin_ev1/MainActivity.kt
+++ app/src/main/java/com/example/practica_kotlin_ev1/MainActivity.kt
@@ -9,11 +9,15 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -57,24 +61,36 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Pantalla_Inicial(modifier: Modifier = Modifier) {
      Column(
-         modifier = modifier
-             .background(Color(0xFFFFF8E7))
-             .padding(15.dp)
-             .fillMaxWidth()
+         modifier = modifier //modificador que afecta a toda la columna
+             .background(Color(0xFFFFF8E7)) //background de toda la pantalla
+             .padding(top = 150.dp)
+             .fillMaxWidth(), //hacer que ocupe toda la pantalla, para que no quede espacio en blanc
+            horizontalAlignment = Alignment.CenterHorizontally,
+
      ) {
-         val nombre = stringResource(id = R.string.nombre)
 
-         Text(
-             "¡Bienvenido a Pizza Time " + nombre + "!",
-             fontSize = 30.sp,
-             textAlign = TextAlign.Center,
-         ) //hacer que ponga el nombre
+        Row( modifier = modifier
+            .background(Color(0xFFFFF8E7)),
+            verticalAlignment = Alignment.CenterVertically) { /*Un row para poder poner la
+            imagen a la izquierda y el texto a la derecha como tenia pensado*/
 
-         Image(
-             painterResource(R.drawable.bobeponja),
-             contentDescription = "imagen de la persona",
-         )
+            Image(
+                painterResource(R.drawable.bobeponja), //la misma imagen
+                contentDescription = "Foto de perfil del usuario", //descripcion de img en caso de que no cargue
+                modifier = Modifier //permitir que podamos modificar propiedades de la img
+                .size(130.dp) //poner tamaño a la img
+                )
 
+            val nombre = stringResource(id = R.string.nombre) //nombre del usuario que tenemos
+            Text(
+                //texto para darle la bienvenida
+                "¡Bienvenido a Pizza Time " + nombre + "!",
+                fontSize = 30.sp,
+                textAlign = TextAlign.Center,
+            )
+        }
+
+         //BOTON REALIZAR PEDIDO
          Button(
              onClick = {/*TODO*/ },
              colors = ButtonDefaults.buttonColors(containerColor = Color.Green)
@@ -85,8 +101,9 @@ fun Pantalla_Inicial(modifier: Modifier = Modifier) {
              )
          }
 
-         Spacer(modifier = Modifier.padding(50.dp)) //separador ou yea
+         Spacer(modifier = Modifier.padding(40.dp)) //separador ou yea
 
+         //BOTON LISTAR PEDIDOS
          Button(
              onClick = {/*TODO*/ },
              colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)
@@ -97,22 +114,24 @@ fun Pantalla_Inicial(modifier: Modifier = Modifier) {
              )
          }
 
-         //Boton de abajo
-         var mostrardatos by remember { mutableStateOf(false) }
+         //BOTON INFORMACION
 
-         if (mostrardatos) {
+         //Script que muestra datos
+         var mostrardatos by remember { mutableStateOf(false) } //hacer que se acuerde y no lo muestre de repente
+         if (mostrardatos) { //si se le da al boton y se habilita :
+            //*vals para poner el nombre apellido gmail y telefono
              val nombre = stringResource(id = R.string.nombre)
             val apellidos = stringResource(id = R.string.apellido)
              val gmail = stringResource(id = R.string.gmail)
              val telefono = stringResource(id = R.string.num)
 
-             AlertDialog(
+             AlertDialog( //Ventana emergente para que quede mucho mejor
                  onDismissRequest = { mostrardatos = false },
-                 confirmButton = {
-                     Button(onClick = { mostrardatos = false }) {
+                 confirmButton = { //Boton para que confirme y se quite
+                     Button(onClick = { mostrardatos = false }) { //que se quite si lo acepta
                          Text("Aceptar")
                      }
-                 },
+                 }, //contenido de la ventana:
                  title = { Text("Datos del usuario") },
                  text = { Text("Nombre: "+nombre+"\n" +
                          "Apellidos: "+apellidos+"\n" +
@@ -121,9 +140,18 @@ fun Pantalla_Inicial(modifier: Modifier = Modifier) {
              )
          }
 
-         Button(onClick = { mostrardatos = true }, colors = ButtonDefaults.buttonColors(containerColor = Color.Blue)) {
-                Text("i")
-
+         Box( //para poder alinear un boton en concreto hay que meterlo en una caja
+             modifier = Modifier.fillMaxSize() // hacer que llegue a esquina
+         ) {
+             Button(
+                 onClick = { mostrardatos = true }, //habilita el script de mostrar datos de arriba
+                 modifier = Modifier
+                     .align(Alignment.BottomEnd) //ponerlo al fondo derecha (podria borrar esto la vdd)
+                     .padding(bottom = 200.dp), //subirlo al centro
+                 colors = ButtonDefaults.buttonColors(Color(0xCC2CA49F))
+             ) {
+                 Text("Mis Datos")
+             }
          }
      }
 }
